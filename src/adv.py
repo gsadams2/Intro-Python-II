540f9159c691a3114f0d8cc8e5fd0e3d167d9209
George Adams <georgeadams@DRs-MacBook-Pro.local>
2019-10-03 16:53:26 -0500

    started on day 2 stuff. added item class. added a lot of the methods. can't get it to work but trying out a few options

--- src/adv.py
+++ src/adv.py
@@ -1,9 +1,8 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
-
-
 
 room = {
     'outside':  Room("Outside Cave Entrance",
@@ -23,31 +22,31 @@ to north. The smell of gold permeates the air."""),
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
-
-
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
+
+room['foyer'].add_item(Item('spectacles', 'these are glosses used to find hidden passages & items'))
+room['foyer'].add_item(Item('heineken', 'drink a heineken for super strength'))
+
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
 room['overlook'].s_to = room['foyer']
+
+room['overlook'].add_item(Item('cloak', 'makes you invisible'))
+
 room['narrow'].w_to = room['foyer']
+
+room['narrow'].add_item(Item('gun', 'use it to shoot your enemies'))
+
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
+room['treasure'].add_item(Item('phone', 'use the phone to call your mommy'))
+
 # Main
-#
-
 # Make a new player object that is currently in the 'outside' room.
-
-
-
-
 # Write a loop that:
-#
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
@@ -56,14 +55,18 @@ room['treasure'].s_to = room['narrow']
     #print(bernard.current_room.name)
 
 
-
 bernard = Player("Bernard", room["outside"])
 
 print(bernard)
 
 command = input(' Move in a direction (N, S, E, W) or hit q to quit: \n')
 
+
+split_command = command.split()
+
 directions = ['n', 's', 'e', 'w']
+
+
 
 while not command == 'q':
     if any(i in command for i in directions):
@@ -71,12 +74,18 @@ while not command == 'q':
         print(bernard)
         print("Do you wish to enter another room?")
         command = input(' Move in a direction (N, S, E, W) or hit q to quit: \n')
+    elif len(split_command) == 2:
+        if split_command[0] == 'grab':
+            bernard.on_grab(split_command[1])
+        elif split_command[0] == 'drop':
+            bernard.on_drop(split_command[1])
+            
     else:
         print("invalid brooooo")
         print("Do you wish to enter another room?")
         command = input(' Move in a direction (N, S, E, W) or hit q to quit: \n')
         
-
+#Add functionality to the main loop that prints out all the items that are visible to the player when they are in that room.
 
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
--- src/player.py
+++ src/player.py
@@ -5,6 +5,7 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
     
     def __str__(self):
         return f"Hello {self.name}, you are in the {self.current_room.name}. Also... {self.current_room.description} \n"
@@ -30,5 +31,38 @@ class Player:
             return print("Broooooo, that's not even a valid direction")
         return self.current_room
 
+    def on_grab(self, item_name):
+        item = self.current_room.find_item(item_name)
+        if item is None:
+            print("No item here")
+        else:
+            self.inventory.append(item)
+            self.current_room.remove_item(item)
+            print(f'You have picked up {item_name}')
+
+    def on_drop(self, item_name):
+        item = self.current_room.find_item(item_name)
+        if item is None:
+            print("No item here")
+        else:
+            self.inventory.remove(item)
+            self.current_room.add_item(item)
+            print(f'You have dropped {item_name}')
+
+    def check_inventory(self, item_name):
+        for item in self.inventory:
+            if item.name == item_name:
+                return item
+        return None
+
+    def remove_item(self, item):
+        self.inventory.remove(item)
+    
+        if len(self.inventory) == 0:
+            print("No items in your inventory brooooooo")
+        else:
+            print("Items:")
+            for i in self.inventory:
+                print(i.name)
 
 
